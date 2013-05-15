aa51c1ce8aea04dfeaf8d698805e5571e21fb7a7
KiselN <nikita_lk@yahoo.com>
2013-05-15 22:13:18 +0200

    add keynodes for oriented and not oriented graph and edge

--- sc-graph/sc_graph_keynodes.c
+++ sc-graph/sc_graph_keynodes.c
@@ -4,11 +4,16 @@
 #include <stdio.h>
 
 sc_addr sc_graph_keynode_graph;
+sc_addr sc_graph_keynode_not_oriented_graph;
+sc_addr sc_graph_keynode_oriented_graph;
 sc_addr sc_graph_keynode_rrel_vertex;
 sc_addr sc_graph_keynode_rrel_arc;
 
 const char sc_graph_keynode_graph_str[] = "graph_graph";
+const char sc_graph_keynode_not_oriented_graph_str[] = "graph_not_oriented_graph";
+const char sc_graph_keynode_oriented_graph_str[] = "graph_oriented_graph";
 const char sc_graph_keynode_rrel_vertex_str[] = "graph_rrel_vertex";
+const char sc_graph_keynode_rrel_edge_str[] = "graph_rrel_edge";
 const char sc_graph_keynode_rrel_arc_str[] = "graph_rrel_arc";
 
 
@@ -24,7 +29,10 @@ sc_bool sc_graph_initialize_keynodes()
 {
     // try to resolve keynode
     resolve_keynode(sc_graph_keynode_graph, sc_type_node_struct);
+    resolve_keynode(sc_graph_keynode_not_oriented_graph, sc_type_node_struct);
+    resolve_keynode(sc_graph_keynode_oriented_graph, sc_type_node_struct);
     resolve_keynode(sc_graph_keynode_rrel_vertex, sc_type_node_role);
+    resolve_keynode(sc_graph_keynode_rrel_edge, sc_type_node_role);
     resolve_keynode(sc_graph_keynode_rrel_arc, sc_type_node_role);
 
     return SC_TRUE;
